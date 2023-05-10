4248bb4315ed3356e7e7868044268829880d7cde
Nayyar Abbas <abbasnayyar63@gmail.com>
2023-05-10 14:07:53 +0500

    index.js synchronous readfile

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ import fs from "fs";
 const app = express();
 const port = 3000;
 app.use("/", (req, res) => {
-    fs.readFile("./members.json", "utf8", (err, jsonString) => {
-        if (err) {
-            console.log("File read failed:", err);
-            return;
-        }
-        try {
-            const jsonData = JSON.parse(jsonString);
-            res.json(jsonData)
-        } catch (err) {
-            console.log("Error parsing JSON string:", err);
-            return;
-        }
-    });
-    // try {
-    //     const jsonString = fs.readFileSync('./members.json');
-    //     const jsonData = JSON.parse(jsonString);
-    // } catch (err) {
-    //     console.log("Error parsing JSON string:", err);
-    //     return;
-    // }
+    // fs.readFile("./members.json", "utf8", (err, jsonString) => {
+    //     if (err) {
+    //         console.log("File read failed:", err);
+    //         return;
+    //     }
+    //     try {
+    //         const jsonData = JSON.parse(jsonString);
+    //         res.json(jsonData)
+    //     } catch (err) {
+    //         console.log("Error parsing JSON string:", err);
+    //         return;
+    //     }
+    // });
+    try {
+        const jsonString = fs.readFileSync('./members.json');
+        const jsonData = JSON.parse(jsonString);
+        res.json(jsonData)
+    } catch (err) {
+        console.log("Error parsing JSON string:", err);
+        return;
+    }
 })
 app.listen(port, () => {
     console.log(`listening at port ${port}`)
